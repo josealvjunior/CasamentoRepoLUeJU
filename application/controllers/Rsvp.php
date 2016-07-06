73ca20d8a2747ee2a4a844df031b4227581c5fa2
Juninho <josealvjunior@gmail.com>
2016-07-05 22:25:27 -0300

    email confirmacao

--- application/controllers/Rsvp.php
+++ application/controllers/Rsvp.php
@@ -51,7 +51,7 @@ class Rsvp extends CI_Controller {
                 <p>Muito obrigado por confirmar sua presença e de '. $data['numpessoas'].' em nosso casamento!<br /> 
                 Não se esqueça da data e horário:<br />
                 A cerimônia será dia 13/08/2016 as 16h na Paróquia de Santa Efigênia dos Militares - R. Álvares Maciel, 223.<br />
-                ***Haverá conferênicia nominal na portaria então leve um documento de identidade!!!****<br />
+                ***Haverá conferênicia nominal na portaria da recepção, então leve um documento de identidade!!!****<br />
                 Sua presença é muito importante para nós! Agradecemos o carinho e nos vemos lá!<br /> 
                 Abraços!<br /> 
                 Luana & Juninho.</p>'
@@ -62,7 +62,7 @@ class Rsvp extends CI_Controller {
                 <p>Muito obrigado por confirmar sua presença em nosso casamento!<br /> 
                 Não se esqueça da data e horário:<br />
                 A cerimônia será dia 13/08/2016 as 16h na Paróquia de Santa Efigênia dos Militares - R. Álvares Maciel, 223.<br />
-                <b>***Haverá conferênicia nominal na portaria então leve um documento de identidade!!!****</b><br />
+                <b>***Haverá conferênicia nominal na portaria da recepção, então leve um documento de identidade!!!****</b><br />
                 Sua presença é muito importante para nós! Agradecemos o carinho e nos vemos lá!<br /> 
                 Abraços!<br /> 
                 Luana & Juninho.</p>'
